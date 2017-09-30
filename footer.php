e3a1de846c4fe4a4f986bef98a5002817aab80b4
Onofri Camila <onofricamila2@gmail.com>
2017-09-30 19:59:06 -0300

    bases para pensar en php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer class="footer-div">
+        <div>
+            <p> ©<a href="https://www.proyecto2017.linti.unlp.edu.ar/"> Proyecto de Software 2017</a>- Aplicación web para el
+                hospital Dr. Ricardo Gutiérrez.</p>
+        </div>
+</footer>
+
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="js/materialize.min.js"></script>
