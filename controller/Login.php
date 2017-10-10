2f40c0a389bb5c4cd668d9bc0ed8638f2b37384b
Sebastian <sebastianraimondi@gmail.com>
2017-10-10 19:16:47 -0300

    navbar html to php in login controller

--- controller/Login.php
+++ controller/Login.php
@@ -21,7 +21,7 @@
             }
             else {
                 require_once 'view/header.html';
-                require_once 'view/navbar.html';
+                require_once 'view/navbar.php';
                 require_once 'view/login/login.html';
                 require_once 'view/footer.html';
             }
