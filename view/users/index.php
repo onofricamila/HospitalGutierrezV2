6f284a5f8bbd3b9a39fa538cd542fa8840acb7a5
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 02:28:58 -0300

    users index dropdown fix2

--- view/users/index.php
+++ view/users/index.php
@@ -21,7 +21,7 @@
                 </div>
 
                 <!-- Dropdown Trigger -->
-                <a class='dropdown-button btn' href='#' data-activates='dropdown1'>Acciones</a>
+                <a class='dropdown-button btn' href='#' data-activates="<?php echo "dropdown".$i ?>">Acciones</a>
 
                 <!-- Dropdown Structure -->
                 <ul id="<?php echo "dropdown".$i ?>" class='dropdown-content'>
