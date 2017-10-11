e632916d5c73a805ceca1f4348dd6c55c0a033fd
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 15:31:15 -0300

    test no col for search

--- view/users/index.php
+++ view/users/index.php
@@ -16,7 +16,7 @@
             </div>
         </div>
             
-        <div class="col s10 l4">
+        <div class="">
             <div class="input-field">
                 <input id="search" type="text">
                 <label for="search">Nombre</label>
