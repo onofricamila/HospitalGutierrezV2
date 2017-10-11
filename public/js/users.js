7a34007ac718afcc70c6e3abcaf6f16097b7f970
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 15:21:34 -0300

    removed jquery function

--- public/js/users.js
+++ public/js/users.js
@@ -1,18 +1,4 @@
 //jQuery name space
-(function ($) {
-    //document ready
-    $(function () {
-        $(".dropdown-button").dropdown({
-            hover: true,
-            inDuration: 300,
-            outDuration: 225,
-            constrain_width: false, // Does not change width of dropdown to that of the activator
-            // gutter: -130, // Spacing from edge
-            belowOrigin: false, // Displays dropdown below the button
-            alignment: 'left'
-        });
-    }); // end of document ready
-})
 $(document).ready(function() {
     $('select').material_select();
 });
