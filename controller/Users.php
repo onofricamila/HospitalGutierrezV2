b95edee7db8bf00578c9e9b96c8c3b9b3e856197
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 00:38:44 -0300

    changed stylesheet index users

--- controller/Users.php
+++ controller/Users.php
@@ -23,7 +23,7 @@ class UsersController {
         }
 
         if ($users = User::all()) {
-            require_once 'view/header.html';
+            require_once 'view/users/header.html';
             require_once 'view/navbar.php';
             require_once 'view/users/index.php';
             require_once 'view/footer.html';
