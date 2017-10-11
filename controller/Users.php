62f5ff2269b24547e9de38acd9bfd79c79c32cc0
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 18:39:06 -0300

    fix newuser

--- controller/Users.php
+++ controller/Users.php
@@ -62,11 +62,11 @@ class UsersController {
             die;
         }
 
-        if ((!isset($_POST['email']) || $email = trim($_POST['email']) == "")
-            || (!isset($_POST['user']) || $user = trim($_POST['user']) == "")
-            || (!isset($_POST['pass']) || $pass = trim($_POST['pass']) == "")
-            || (!isset($_POST['first_name']) || $first_name =trim($_POST['first_name']) == "")
-            || (!isset($_POST['last_name'])) || $last_name = trim($_POST['last_name']) == "")
+        if ((!isset($_POST['email']) || ($email = trim($_POST['email'])) == "")
+            || (!isset($_POST['user']) || ($user = trim($_POST['user'])) == "")
+            || (!isset($_POST['pass']) || ($pass = trim($_POST['pass'])) == "")
+            || (!isset($_POST['first_name']) || ($first_name =trim($_POST['first_name'])) == "")
+            || (!isset($_POST['last_name'])) || ($last_name = trim($_POST['last_name'])) == "")
         {
             echo 'No llenaste bien los campos';
             die;
