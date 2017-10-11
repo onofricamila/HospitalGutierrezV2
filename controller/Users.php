3a95e0cd72443d9024b5f8ac93f79c480bbc7b02
Sebastian <sebastianraimondi@gmail.com>
2017-10-10 23:52:05 -0300

    UsersController base

--- controller/Users.php
+++ controller/Users.php
@@ -1,5 +1,22 @@
 <?php
 
 class UsersController {
+    private static $instance;
     
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct() {
+
+    }
+    
+    public function index(){
+        
+    }
+
 }
