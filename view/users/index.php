5e7269211635975e309e673c2ed8c2ecf10cb298
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 02:36:25 -0300

    revert last change

--- view/users/index.php
+++ view/users/index.php
@@ -18,23 +18,23 @@
                     <p>Es activo?: <?php if ($user->active) { echo 'Si'; } else { echo 'No'; } ?></p>
                     <p>Fecha de actualizacion: <?php echo $user->updated_at; ?></p>
                     <p>Fecha de creacion: <?php echo $user->created_at; ?></p>
-
-                    <!-- Dropdown Trigger -->
-                    <a class='dropdown-button btn' href='#' data-activates="<?php echo "dropdown".$i ?>">Acciones</a>
-
-                    <!-- Dropdown Structure -->
-                    <ul id="<?php echo "dropdown".$i ?>" class='dropdown-content'>
-                        <li><a href="<?php echo "?controller=Users&action=togglestate&id=".$user->id; ?>" target="_self"><?php if ($user->active) { echo 'Bloquear Usuario'; } else { echo 'Activar Usuario'; } ?></a></li>
-                        <li class="divider"></li>
-                        <li><a href="<?php echo "?controller=Users&action=deleteUser&id=".$user->id; ?>" target="_self">Eliminar Usuario</a></li>
-                        <li class="divider"></li>
-                        <li><a href="<?php echo "?controller=Users&action=updateUser&id=".$user->id; ?>" target="_self">Actualizar Usuario</a></li>
-                        <li class="divider"></li>
-                        <li><a href="<?php echo "?controller=Users&action=showUser&id=".$user->id; ?>" target="_self">Ver Usuario</a></li>
-                        <li class="divider"></li>
-                        <li><a href="<?php echo "?controller=Users&action=adminRoles&id=".$user->id; ?>" target="_self">Administrar Roles</a></li>
-                    </ul>
                 </div>
+
+                <!-- Dropdown Trigger -->
+                <a class='dropdown-button btn' href='#' data-activates="<?php echo "dropdown".$i ?>">Acciones</a>
+
+                <!-- Dropdown Structure -->
+                <ul id="<?php echo "dropdown".$i ?>" class='dropdown-content'>
+                    <li><a href="<?php echo "?controller=Users&action=togglestate&id=".$user->id; ?>" target="_self"><?php if ($user->active) { echo 'Bloquear Usuario'; } else { echo 'Activar Usuario'; } ?></a></li>
+                    <li class="divider"></li>
+                    <li><a href="<?php echo "?controller=Users&action=deleteUser&id=".$user->id; ?>" target="_self">Eliminar Usuario</a></li>
+                    <li class="divider"></li>
+                    <li><a href="<?php echo "?controller=Users&action=updateUser&id=".$user->id; ?>" target="_self">Actualizar Usuario</a></li>
+                    <li class="divider"></li>
+                    <li><a href="<?php echo "?controller=Users&action=showUser&id=".$user->id; ?>" target="_self">Ver Usuario</a></li>
+                    <li class="divider"></li>
+                    <li><a href="<?php echo "?controller=Users&action=adminRoles&id=".$user->id; ?>" target="_self">Administrar Roles</a></li>
+                </ul>
             </div>
         </div>
         <?php
