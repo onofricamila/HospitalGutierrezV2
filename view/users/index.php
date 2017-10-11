12aa868a58faceafd7678023e5e0fc1ec755345b
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 00:36:07 -0300

    added echos user index view

--- view/users/index.php
+++ view/users/index.php
@@ -3,13 +3,13 @@
     foreach ($users as $user) {
         ?>
         <div class="container">
-            <p>E-mail: <?php $user->email ?></p>
-            <p>Nombre de usuario: <?php $user->username ?></p>
-            <p>Es activo?: <?php $user->active ?></p>
-            <p>Fecha de actualizacion: <?php $user->updated_at ?></p>
-            <p>Fecha de creacion: <?php $user->created_at ?></p>
-            <p>Nombre: <?php $user->first_name ?></p>
-            <p>Apellido: <?php $user->last_name ?></p>
+            <p>E-mail: <?php echo $user->email; ?></p>
+            <p>Nombre de usuario: <?php echo $user->username; ?></p>
+            <p>Es activo?: <?php echo $user->active; ?></p>
+            <p>Fecha de actualizacion: <?php echo $user->updated_at; ?></p>
+            <p>Fecha de creacion: <?php echo $user->created_at; ?></p>
+            <p>Nombre: <?php echo $user->first_name; ?></p>
+            <p>Apellido: <?php echo $user->last_name; ?></p>
         </div>
         <?php
     }
