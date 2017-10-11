f43d59c12c28bfe7cfd24b1c3fdaccb6fb19e881
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 16:26:17 -0300

    button test

--- view/users/index.php
+++ view/users/index.php
@@ -7,7 +7,9 @@
         <form class="search-form" method="post" action"?controller=Users&action=index" target="_self">
 
         <div>
-            <input type="submit"><i class="material-icons">search</i>
+            <button type="submit">
+                <i class="material-icons">search</i>
+            </button>
         </div>
 
         <div class="col s3 l2" style="height: 100%;">
