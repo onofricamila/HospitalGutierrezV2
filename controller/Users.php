e50ca33d71c056ea3adeb3bd026b7f6022ed37ea
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 00:32:38 -0300

    prueba index user

--- controller/Users.php
+++ controller/Users.php
@@ -23,10 +23,10 @@ class UsersController {
         }
 
         if ($users = User::all()) {
-            require_once 'view/header.html';
-            require_once 'view/navbar.php';
-            require_once '/view/users/index.php';
-            require_once 'view/footer.html';
+            echo "Bien";
+        }
+        else {
+            echo "Error";
         }
     }
 
