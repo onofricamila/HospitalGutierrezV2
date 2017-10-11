87b661fe1105325f03467d04e7cb901500bbc0b5
Onofri Camila <onofricamila2@gmail.com>
2017-10-11 15:37:14 -0300

    fx paciente ready

--- model/Paciente.php
+++ model/Paciente.php
@@ -31,7 +31,49 @@
                 ->telefono = $array['telefono']
                 ->idObraSocial = $array['idObraSocial'];
         }
-
        
+        public static function newPaciente($apellido, $nombre, $fecha_nacimiento, $idGenero, $idTipoDoc, $dni, $telefono, $idObraSocial, $domicilio) {
+            $connection = Connection::getInstance();
+    
+            $query = $connection->prepare("INSERT INTO usuario (apellido, nombre, fecha_nacimiento, idGenero, idTipoDoc, dni, telefono, idObraSocial, domicilio) VALUES (?, ?, ?, ?, ?, ?)");
+            $query->execute(array($apellido, $nombre, $fecha_nacimiento, $idGenero, $idTipoDoc, $dni, $telefono, $idObraSocial, $domicilio));
+            
+            return $query->rowCount() == 1;
+        }
+    
+        public function updatePaciente($idPaciente, $apellido, $nombre, $fecha_nacimiento, $idGenero, $idTipoDoc, $dni, $telefono, $idObraSocial, $domicilio) {
+            $connection = Connection::getInstance();
+            
+            $query = $connection->prepare("UPDATE usuario SET apellido = ". $apellido . ", nombre = ". $nombre . ", fecha_nacimiento = ". $fecha_nacimiento . ", idGenero = ". $idGenero . ", idTipoDoc = ". $idTipoDoc . ", dni = ". $dni . ", telefono = ". $telefono . ", idObraSocial = ". $idObraSocial . ", domicilio = ". $domicilio . " WHERE idPaciente=?");
+            $query->execute(array($this->idPaciente));
+    
+            return $query->rowCount() == 1;
+        }
+
+        public static function deletePaciente($idPaciente) {
+            $connection = Connection::getInstance();
+    
+            $query = $connection->prepare("DELETE FROM paciente WHERE idPaciente=?");
+            $query->execute(array($idPaciente));
+            
+            return $query->rowCount() == 1;
+        }
+
+        public static function all() {
+            $connection = Connection::getInstance();
+    
+            $result = $connection->query("SELECT * FROM paciente");
+    
+            $pacientes = [];
+    
+            if ($result->rowCount() > 0) {
+                while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                    $pacientes[] = new Paciente($row);
+                }
+                return $pacientes;
+            }
+            return false;
+        }
+    
     }
 ?>
