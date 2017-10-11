9b64ac75e93114a925cbef7e060953e8e701db54
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 00:56:49 -0300

    index users update

--- view/users/index.php
+++ view/users/index.php
@@ -1,15 +1,22 @@
-<section class="row">
+<section class="container">
     <?php
     foreach ($users as $user) {
         ?>
-        <div class="container">
-            <p>E-mail: <?php echo $user->email; ?></p>
-            <p>Nombre de usuario: <?php echo $user->username; ?></p>
-            <p>Es activo?: <?php echo $user->active; ?></p>
-            <p>Fecha de actualizacion: <?php echo $user->updated_at; ?></p>
-            <p>Fecha de creacion: <?php echo $user->created_at; ?></p>
-            <p>Nombre: <?php echo $user->first_name; ?></p>
-            <p>Apellido: <?php echo $user->last_name; ?></p>
+        <div class="col s12 m6 l4">
+            <div class="card">
+                <div class="card-content white-text">
+                    <span class="card-title"><?php echo $user->last_name.", ".$user->first_name; ?></span>
+                    <p>E-mail: <?php echo $user->email; ?></p>
+                    <p>Nombre de usuario: <?php echo $user->username; ?></p>
+                    <p>Es activo?: <?php echo $user->active; ?></p>
+                    <p>Fecha de actualizacion: <?php echo $user->updated_at; ?></p>
+                    <p>Fecha de creacion: <?php echo $user->created_at; ?></p>
+                </div>
+                <div class="card-action">
+                    <a href=""><?php if ($user->active) { echo 'Bloquear'; } else { echo 'Activar'; } ?></a>
+                    <a href="">Administrar roles</a>
+                </div>
+            </div>
         </div>
         <?php
     }
