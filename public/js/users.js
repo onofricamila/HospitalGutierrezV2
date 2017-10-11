e898bb4f5af6dd6e7dc7206acd63191cd7162dc2
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 17:45:24 -0300

    modal fix

--- public/js/users.js
+++ public/js/users.js
@@ -16,8 +16,8 @@
 $(document).ready(function () {
     $('select').material_select();
 });
-$(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-});
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 (jQuery); // end of jQuery name space
