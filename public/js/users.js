e839f6d195fc25b6b8cb14c76dbe37b87331c42f
Sebastian <sebastianraimondi@gmail.com>
2017-10-11 23:42:32 -0300

    removed test

--- public/js/users.js
+++ public/js/users.js
@@ -24,7 +24,6 @@ $('.rolesModalTrigger').click(function(){
     modal = $('#rolesModal');
     contenido = $(this).closest('div').find('.contenido-card');
     id = contenido.children().eq(0).text();
-    alert(id);
     $('#rolesModalId').val(id);
     user = contenido.children().eq(2).text().replace('Nombre de usuario: ', '');
     $('#rolesModalUser').text(user);
