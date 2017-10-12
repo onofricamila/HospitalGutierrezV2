3b921966ff8d273865330b0618818471831ce74b
Onofri Camila <onofricamila2@gmail.com>
2017-10-12 17:39:30 -0300

    limit date picker

--- public/js/pacientes.js
+++ public/js/pacientes.js
@@ -149,8 +149,7 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
+    selectYears: 70, // Creates a dropdown of 15 years to control year,
     clear: 'Clear',
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
@@ -158,6 +157,5 @@ $(document).ready(function() {
 });
 
 $('.datepicker').pickadate({
-    min: [1917,1,1],
     max: Date.now()
   })
