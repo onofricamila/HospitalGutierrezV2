59b87b2f63d5ee27007a760cf4a1906849c226eb
Onofri Camila <onofricamila2@gmail.com>
2017-10-12 16:48:05 -0300

    daet picker js

--- public/js/pacientes.js
+++ public/js/pacientes.js
@@ -144,3 +144,13 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('select').material_select();
   });
+
+/* para el date picker */
+$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year,
+    today: 'Today',
+    clear: 'Clear',
+    close: 'Ok',
+    closeOnSelect: false // Close upon selecting a date,
+  }); 
