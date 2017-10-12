734176fed68031552e324a9a436fbdfc986f317a
Onofri Camila <onofricamila2@gmail.com>
2017-10-12 19:13:48 -0300

    Auto stash before merge of "master" and "origin/master"

--- view/pacientes/pacientes.php
+++ view/pacientes/pacientes.php
@@ -8,9 +8,9 @@
         <br>
         <a href="?controller=Pacientes&action=showPaciente&idPaciente=2">Show patient demo</a>
         <br>
-        <a href="?controller=Pacientes&action=deletePaciente&idPaciente=3">Show bai patient demo</a>
+        <a href="?controller=Pacientes&action=deletePaciente&idPaciente=">Show bai patient demo (funciona)</a>
         <br>
-        <a href="?controller=Pacientes&action=showPaciente">Show new patient demo</a>
+        <a href="?controller=Pacientes&action=newPaciente">Show new patient demo</a>
         <br>
         <a href="?controller=Pacientes&action=updatePaciente&idPaciente=2">Show edit patient demo</a>
         <br>
