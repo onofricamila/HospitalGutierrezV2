4bd2986e67fa202002a172092abc43b02c4d01df
Sebastian <sebastianraimondi@gmail.com>
2017-10-12 18:44:59 -0300

    HomeController to twig

--- controller/Home.php
+++ controller/Home.php
@@ -12,11 +12,28 @@
 
         private function __construct() {
         }
-        
+
+        public function getContext() {
+            $config = ConfigController::getConfiguration();
+            $context = [];
+            $context['descripcion1'] = $config['descripcion1'];
+            $context['descripcion2'] = $config['descripcion2'];
+            $context['descripcion3'] = $config['descripcion3'];
+            return $context;
+        }
+
         public function index(){
+
+            $path = '/home/view.html.twig';
+            $context = $this->getContext();
+            TwigController::renderTwig($path, $context);
+            die;
+
+            /*
             require_once 'view/header.html';
             require_once 'view/navbar.php';
             require_once 'view/home/home.html';
             require_once 'view/footer.html';
+            */
         }
     }
