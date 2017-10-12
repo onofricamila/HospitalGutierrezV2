fc22799524b032ebced053eeac13a25814a3ad22
Sebastian <sebastianraimondi@gmail.com>
2017-10-12 19:20:04 -0300

    prueba utf8

--- Connection.php
+++ Connection.php
@@ -13,7 +13,7 @@
 				$db_pass="MTU5ZGM1MjFhMmJk";
 				$db_base="grupo46";
 			
-				self::$instance = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass);
+				self::$instance = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 			}
 			return self::$instance;
 		}
