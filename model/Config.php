eee62b28f5c5c435925d13babdea4c3e9cd0963a
Sebastian <sebastianraimondi@gmail.com>
2017-10-12 18:23:26 -0300

    separated instance variables

--- model/Config.php
+++ model/Config.php
@@ -1,6 +1,7 @@
 <?php
 class Configuration {
-    private static $instance, $id, $titulo, $descripcion1, $descripcion2, $descripcion3, $email, $elementos, $mantenimiento;
+    private static $instance;
+    public $id, $titulo, $descripcion1, $descripcion2, $descripcion3, $email, $elementos, $mantenimiento;
     
     public static function getInstance() {
         if (!isset(self::$instance)) {
