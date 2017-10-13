aaa1de35412bab8f129b3551f22aa42d48f91f52
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 01:49:41 -0300

    fix if

--- controller/Config.php
+++ controller/Config.php
@@ -59,6 +59,7 @@ class ConfigController {
         }
 
         $context = $this->getCardInfo();
+        $context['mantenimiento'] = $this->mantenimiento;
         $path = '/config/admin.html.twig';
         $context['stylesheets'] = ['/public/css/config-admin.css'];
         $context['javascripts'] = ['/public/js/config-admin.js'];
