d3d1397ffc276041c5c4e98d1484189add5da572
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 00:52:03 -0300

    fix textarea

--- public/js/config-admin.js
+++ public/js/config-admin.js
@@ -1,4 +1,7 @@
 $('.updateTrigger').click(function () {
+    $("textarea").each(function(textarea) {
+        $this.height( $this[0].scrollHeight );
+    });
     $('.input-field').show();
     $('.data').hide();
     $('.updateTrigger').hide();
