ef0e9d6ed81a7649f8984ed397d757778dd72233
Camila Onofri <onofricamila2@gmail.com>
2017-10-13 19:45:35 -0300

    try

--- controller/Pacientes.php
+++ controller/Pacientes.php
@@ -50,7 +50,7 @@
                 $context = [];
                 
                 $context['stylesheets'] = ['/public/css/pacientes.css'];
-                $context['javascripts'] = ['/public/js/users.js'];
+                $context['javascripts'] = ['/public/js/pacientes.js'];
                 $context['pagename'] = 'Pacientes - Index';
                 $context['allPaciente'] = $allPaciente;
                 $context['allTipoDoc'] = TipoDoc::all();
