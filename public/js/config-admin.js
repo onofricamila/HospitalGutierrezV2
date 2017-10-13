b65919903eaa22b26afa204e6a6e664be15ad702
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 00:27:35 -0300

    triggers show hide

--- public/js/config-admin.js
+++ public/js/config-admin.js
@@ -1,10 +1,16 @@
 $('.updateTrigger').click(function () {
     $('.input-field').show();
     $('.data').hide();
+    $('.updateTrigger').hide();
+    $('.backTrigger').show();
+    $('.submitTrigger').show();
     Materialize.updateTextFields();
 });
 $('.backTrigger').click(function () {
     $('.input-field').hide();
     $('.data').show();
+    $('.updateTrigger').hide();
+    $('.backTrigger').show();
+    $('.submitTrigger').show();
     Materialize.updateTextFields();
 });
