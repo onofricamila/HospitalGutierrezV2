e004fb06656088015811c9290b8fb50fff2c921e
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 00:41:51 -0300

    fix show hide

--- public/js/config-admin.js
+++ public/js/config-admin.js
@@ -2,15 +2,13 @@ $('.updateTrigger').click(function () {
     $('.input-field').show();
     $('.data').hide();
     $('.updateTrigger').hide();
-    $('.backTrigger').show();
-    $('.submitTrigger').show();
+    $('.updateButton').show();
     Materialize.updateTextFields();
 });
 $('.backTrigger').click(function () {
     $('.input-field').hide();
     $('.data').show();
-    $('.updateTrigger').hide();
-    $('.backTrigger').show();
-    $('.submitTrigger').show();
+    $('.updateTrigger').show();
+    $('.updateButton').hide();
     Materialize.updateTextFields();
 });
