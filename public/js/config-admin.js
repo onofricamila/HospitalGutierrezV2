a9c7ff6bf3990a6e1454281f59b37431faaf76bf
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 00:57:25 -0300

    removed scroll

--- public/js/config-admin.js
+++ public/js/config-admin.js
@@ -4,11 +4,6 @@ $('.updateTrigger').click(function () {
             this.style.height = (this.scrollHeight + 10) + 'px';
         });
     });
-    $(window).load(function () {
-        $("html, body").animate({
-            scrollTop: $(document).height()
-        }, 1000);
-    });
     $('.input-field').show();
     $('.data').hide();
     $('.updateTrigger').hide();
