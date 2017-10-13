9d7d5ec7cb0fbd275835b6f9506c156f437cae3c
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 00:26:13 -0300

    js triggers

--- public/js/config-admin.js
+++ public/js/config-admin.js
@@ -1,17 +1,10 @@
-document.getElementById("form").reset();
-$('#form').attr('action', 'index.php?controller=Users&action=updateUser');
-
-contenido = $(this).closest('div').find('.contenido-card');
-id = contenido.children().eq(0).text();
-email = contenido.children().eq(1).text().replace('E-mail: ', '');
-user = contenido.children().eq(2).text().replace('Nombre de usuario: ', '');
-nombre = contenido.children().eq(7).text();
-apellido = contenido.children().eq(8).text();
-
-$('#title').text('Actualizar usuario: ' + user);
-$('#email').val(email);
-$('#user').val(user);
-$('#first_name').val(nombre);
-$('#last_name').val(apellido);
-$('#id').val(id);
-Materialize.updateTextFields();
+$('.updateTrigger').click(function () {
+    $('.input-field').show();
+    $('.data').hide();
+    Materialize.updateTextFields();
+});
+$('.backTrigger').click(function () {
+    $('.input-field').hide();
+    $('.data').show();
+    Materialize.updateTextFields();
+});
