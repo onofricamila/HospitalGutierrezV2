8296ad0ed1efbe81f60f736e264c6af3ccec3c46
Sebastian <sebastianraimondi@gmail.com>
2017-10-13 14:33:38 -0300

    prueba

--- public/js/users.js
+++ public/js/users.js
@@ -69,6 +69,8 @@ $(document).ready(function () {
     var paginacion = $('.paginacion').text();
     $('.cardContainer').each(function( index ) {
         if (index > $('.paginacion').text()) {
+            alert(index);
+            alert($('.paginacion').text())
             $(this).hide();
         }
     });
