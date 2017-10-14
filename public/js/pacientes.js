276d9fd06334aa8992f1db1d5f427c025386ca28
Camila Onofri <onofricamila2@gmail.com>
2017-10-14 20:59:37 -0300

    js

--- public/js/pacientes.js
+++ public/js/pacientes.js
@@ -205,8 +205,12 @@ function nan(num){
 }    
 
 function nas(text){
-    var reg = /^[A-Za-z ]+$/;
-    return (re.test(text));
+    for(i=0;i<text.length;i++){
+        carac=texto.charCodeAt(i);
+        if(carac<48 || carac>57) { // valida numeros
+        return false;
+        }
+    }
 }
 
 /*si el form se envia de todas formas poner en el onsubmit del form event.preventDefault() y si la fx devuelve false no se manda el form */
@@ -246,7 +250,7 @@ function validacion() {
     }
 
     if(nas(nombre) || nas(apellido)){
-        alert('[ERROR] Respeta el formato de numeros para el dni.');
+        alert('[ERROR] Respeta el formato solo letras para el nombre y el apellido.');
         return false;
        }
     
