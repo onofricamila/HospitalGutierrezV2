bb20a520abdcde47b70ec6a5da76edfe98f2dcda
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-07 20:52:58 -0300

    Probando slim

--- Bot/Bot.php
+++ Bot/Bot.php
@@ -1,26 +1,25 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';     // Twig
 
-$botToken = '453768563:AAFJjd8WdUf3fm-SBiIlKpg-HGv0kZdXRmg';
-$website = 'https://api.telegram.org/bot'.$botToken;
-
-$update = file_get_contents('php://input');
-$update = json_decode($update, true);
-
-$chatId = $update['message']['chat']['id'];
-$message = $update['message']['text'];
-
-function sendMessage($chatId, $message)
-{
-    $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
-    file_get_contents($url);
-}
-
-
 // instantiate the App object
 $app = new \Slim\App();
 // Add route callbacks
-$app->get('/', function ($request, $response, $args) {
+$app->get('/bot', function ($request, $response, $args) {
+    $botToken = '453768563:AAFJjd8WdUf3fm-SBiIlKpg-HGv0kZdXRmg';
+    $website = 'https://api.telegram.org/bot'.$botToken;
+
+    $update = file_get_contents('php://input');
+    $update = json_decode($update, true);
+
+    $chatId = $update['message']['chat']['id'];
+    $message = $update['message']['text'];
+
+    function sendMessage($chatId, $message)
+    {
+        $url = $GLOBALS[website]."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+        file_get_contents($url);
+    }
+
     switch ($message) {
         case '/test':
             sendMessage($chatId, "test");
