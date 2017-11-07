cf06ee35be0e0a7d89ec2aace5148797cbaa1216
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-07 20:36:01 -0300

    slim test

--- Bot/Bot.php
+++ Bot/Bot.php
@@ -1,4 +1,5 @@
 <?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';     // Twig
 
 $botToken = '453768563:AAFJjd8WdUf3fm-SBiIlKpg-HGv0kZdXRmg';
 $website = 'https://api.telegram.org/bot'.$botToken;
@@ -15,16 +16,12 @@ function sendMessage($chatId, $message)
     file_get_contents($url);
 }
 
-$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-switch ($message) {
-    case '/test':
-        sendMessage($chatId, $actual_link);
-        break;
-    case '/hi':
-        sendMessage($chatId, "Hello!");
-        break;
-    default:
-        sendMessage($chatId, "Command not found");
-        break;
-}
+// instantiate the App object
+$app = new \Slim\App();
+// Add route callbacks
+$app->post('/hi', function ($request, $response, $args) {
+    sendMessage($chatId, "Hola!");
+});
+// Run application
+$app->run();
