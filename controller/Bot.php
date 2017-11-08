68bf876803b4e491a088d39357ac8bdc4e7047d9
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-07 21:49:50 -0300

    bot

--- controller/Bot.php
+++ controller/Bot.php
@@ -19,6 +19,9 @@
             $rawData = file_get_contents('php://input');
             $response = json_decode($rawData, $returnArray);
             $chatId = $response['message']['chat']['id'];
+            $reply = $response['message']['message_id'];
+
+
 
             // Obtener comando (y sus posibles parametros)
             $regExp = '#^(\/[a-zA-Z0-9\/]+?)(\ .*?)$#i';
@@ -32,12 +35,16 @@
                 $params = '';
             }
 
-            self::dispatcher($chatId, $cmd, $params);
+            self::dispatcher($chatId, $cmd, $params, $response);
         }
 
-        private static function sendMessage($chatId, $message)
+        private static function sendMessage($chatId, $message, $reply = "")
         {
-            $url = self::website()."/sendMessage?chat_id=".$chatId."&text=".urlencode($message);
+            if ($reply != "") {
+                $reply = "&reply_to_message_id=".$reply;
+            }
+
+            $url = self::website()."/sendMessage?chat_id=".$chatId.$reply."&text=".urlencode($message);
             file_get_contents($url);
         }
 
@@ -51,17 +58,43 @@
             return 'https://api.telegram.org/bot'.self::token();
         }
 
-        public static function dispatcher($chatId, $cmd, $params)
+        public static function dispatcher($chatId, $cmd, $params, $response)
         {
             switch ($cmd) {
                 case '/test':
-                    self::sendMessage($chatId, "test");
+                    self::sendMessage($chatId, $reply, "test");
                     break;
                 case '/hi':
-                    self::sendMessage($chatId, "Hello!");
+                    self::sendMessage($chatId, $reply, "Hello!");
                     break;
+                case '/start':
+                    $name = $response['message']['from']['first_name'];
+                    $user = $response['message']['from']['username'];
+                    $text = 'Hola '.$name." Usuario: ".$user.'!'.PHP_EOL;
+                    $text .= '¿Como puedo ayudarte? /help';
+                    sendMessage($chatId, $text);
+                    break;
+                case '/help':
+                    $text = 'Los comandos disponibles son estos:' . PHP_EOL;
+                    $text .= '/start Inicializa el bot' . PHP_EOL;
+                    $text .= '/turnos dd-mm-aaaa Muestra los turnos disponibles del día' . PHP_EOL;
+                    $text .= '/reservar dd-mm-aaaa hh:mm Realiza la reserva del turno' . PHP_EOL;
+                    $text .= '/help Muestra esta ayuda flaca';
+                    sendMessage($chatId, $text);
+                    break;
+                case '/reservar':
+                    $text = 'Te confirmamos el turno para:' . PHP_EOL;
+                    $text .= '10:30' . PHP_EOL;
+                    sendMessage($chatId, $text);
+                    break;
+                case '/turnos':
+                    $text = 'Los turnos disponibles son: 10:30 | 11:45 | 15:15';
+                    sendMessage($chatId, $text);
+                    break
                 default:
-                    self::sendMessage($chatId, "Command not found");
+                    $text = 'Lo siento, no es un comando válido.' . PHP_EOL;
+                    $text .= 'Prueba /help para ver la lista de comandos disponibles';
+                    sendMessage($chatId, $text);
                     break;
             }
         }
