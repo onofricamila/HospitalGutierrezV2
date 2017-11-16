fe322721d767a2f1fc9fc7de46295a7254ee3eac
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-16 14:48:04 -0300

    Comienzos 3.3

--- Connection.php
+++ Connection.php
@@ -1,20 +1,26 @@
 <?php
-	class Connection {
-		private static $instance = NULL;
+    class Connection
+    {
+        private static $instance = null;
 
-		private function __construct() {}
+        private function __construct()
+        {
+        }
 
-		private function __clone() {}
+        private function __clone()
+        {
+        }
 
-		public static function getInstance() {
-			if (!isset(self::$instance)) {
-				$db_host="localhost";
-				$db_user="grupo46";
-				$db_pass="MTU5ZGM1MjFhMmJk";
-				$db_base="grupo46";
-			
-				self::$instance = new PDO("mysql:dbname=$db_base;host=$db_host",$db_user,$db_pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-			}
-			return self::$instance;
-		}
-	}
+        public static function getInstance()
+        {
+            if (!isset(self::$instance)) {
+                $db_host="localhost";
+                $db_user="grupo46";
+                $db_pass="MTU5ZGM1MjFhMmJk";
+                $db_base="grupo46";
+
+                self::$instance = new PDO("mysql:dbname=$db_base;host=$db_host", $db_user, $db_pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+            }
+            return self::$instance;
+        }
+    }
