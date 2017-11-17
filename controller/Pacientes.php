274e10621e96c13bfde562d7291ad02da70e611a
Onofri Camila <onofricamila2@gmail.com>
2017-11-17 18:40:59 -0300

    tewt date

--- controller/Pacientes.php
+++ controller/Pacientes.php
@@ -136,7 +136,7 @@
             /* por el date picker ...*/
             var_dump($fecha_nacimiento);
             $formato = 'Y-m-d';
-            $fecha_nacimiento = DateTime::createFromFormat($formato, $fecha_nacimiento);
+            // $fecha_nacimiento = DateTime::createFromFormat($formato, $fecha_nacimiento);
 
             Paciente::newPaciente($apellido, $nombre, $fecha_nacimiento, $idGenero, $idTipoDoc, $dni, $telefono, $idObraSocial, $domicilio, $heladera, $electricidad, $mascota, $idTipoVivienda, $idTipoCalefaccion, $idTipoAgua);
             $this->index();
