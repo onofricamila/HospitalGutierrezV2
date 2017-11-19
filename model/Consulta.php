22b03e1aaec2266d13f43bd7b68e5821e0fcdf1d
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-19 00:32:52 -0300

    test

--- model/Consulta.php
+++ model/Consulta.php
@@ -63,6 +63,7 @@
             $paciente = Paciente::getPaciente($this ->idPaciente);
             $this ->edad = $paciente->edad();
             $this ->semanas = $paciente->semanas($this->fecha);
+            $this ->paciente = $paciente;
         }
 
         public static function newConsulta($idPaciente, $peso, $vacunas_completas, $vacunas_obs, $maduracion_acorde, $maduracion_obs, $examen_fisico_normal, $examen_fisico_obs, $pc, $ppc, $talla, $alimentacion, $obs_grales, $usuario)
