52a22c3e39851aa038be371d575dd51c7f3ade76
Sebastian Raimondi <sebastianraimondi@gmail.com>
2017-11-19 00:06:50 -0300

    test

--- model/Paciente.php
+++ model/Paciente.php
@@ -30,8 +30,7 @@
             }
             $first = DateTime::createFromFormat('Y-m-d', $date1);
             $second = DateTime::createFromFormat('Y-m-d H:i:s', $date2);
-            echo $date1.'date1';
-            echo ' date2'.$date2;
+            var_dump($first, $second);
             die;
             return floor($first->diff($second)->days/7);
         }
