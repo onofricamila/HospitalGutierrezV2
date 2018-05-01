ce49594114fb24b322b25513de89e242b48393d0
Camila Onofri <onofricamila2@gmail.com>
2018-05-01 15:42:42 -0300

    bottom fixed button

--- final/client/src/components/FixedBottomButton/FixedBottomButton.js
+++ final/client/src/components/FixedBottomButton/FixedBottomButton.js
@@ -3,21 +3,17 @@ import Button from 'material-ui/Button';
 import AddIcon from 'material-ui-icons/Add';
 import classes from 'classnames';
 import {Link} from 'react-router-dom';
-import Grid from 'material-ui/Grid';
+import myClasses from './FixedBottomButton.css';
 
 const button = (props) => {
     return (
-        <Grid container>
-            <Grid item xs={4}>
-                <Grid container justify="left">
-                    <Link to={props.path}>
-                        <Button variant="fab" className={classes.fab} color='primary'>
-                            <AddIcon />
-                        </Button>  
-                    </Link>   
-                </Grid>   
-            </Grid>   
-        </Grid>   
+        <div className={myClasses.FixedBottomButton}>
+        <Link to={props.path}>
+            <Button variant="fab" className={classes.fab} color='primary'>
+                <AddIcon />
+            </Button>  
+        </Link> 
+        </div>  
     );
 }
 
