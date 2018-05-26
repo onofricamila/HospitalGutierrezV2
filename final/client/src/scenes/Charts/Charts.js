a3acfa86c69660566bda5779cb4ab55f795316f4
Onofri Camila <onofricamila2@gmail.com>
2018-05-25 23:35:41 -0300

    bases charts

--- final/client/src/scenes/Charts/Charts.js
+++ final/client/src/scenes/Charts/Charts.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Chart from './Chart'
 import Export from './Export'
+import CircularIndeterminate from '../../components/CircularIndeterminate/CircularIndeterminate';
 
 class Charts extends Component{
     state = {
@@ -15,15 +16,76 @@ class Charts extends Component{
                 }
             ],
         },
-        documentTypesData: [],
-        insurancesData: [],
-        houseTypesData: [],
-        waterTypesData: [],
-        heatingTypesData: [],
+        documentTypes: [],
+        insurances: [],
+        houseTypes: [],
+        waterTypes: [],
+        heatingTypes: [],
+        
+    }
+    
+    arrayFromStateField(field){
+        const array = [];
+        Object.values(this.state[field]).forEach(value => {
+            array[value.id] = value.nombre
+        });
+        return array
     }
 
+    getDocumentTypes(){
+        fetch('https://api-referencias.proyecto2017.linti.unlp.edu.ar/' + "tipo-documento")
+          .then(response => response.json())
+          .then(data => { this.setState({ documentTypes: data }) })
+    }
+
+    getInsurances(){
+        fetch('https://api-referencias.proyecto2017.linti.unlp.edu.ar/' + "obra-social")
+          .then(response => response.json())
+          .then(data => { this.setState({ insurances: data }) })
+    }
+
+    getHouseTypes(){
+    fetch('https://api-referencias.proyecto2017.linti.unlp.edu.ar/' + "tipo-vivienda")
+        .then(response => response.json())
+        .then(data => { this.setState({ houseTypes: data }) })
+    }
+
+    getWaterTypes(){
+    fetch('https://api-referencias.proyecto2017.linti.unlp.edu.ar/' + "tipo-agua")
+        .then(response => response.json())
+        .then(data => { this.setState({ waterTypes: data }) })
+    }
+
+    getHeatingTypes(){
+    fetch('https://api-referencias.proyecto2017.linti.unlp.edu.ar/' + "tipo-calefaccion")
+        .then(response => response.json())
+        .then(data => { this.setState({ heatingTypes: data, loading: false }) })
+    }
+
+    getdocumentTypesData(){
+        
+    }
+
+    componentDidMount(){
+        this.getdocumentTypesData()
+        this.getInsurances()
+        this.getHouseTypes()
+        this.getWaterTypes()
+        this.getHeatingTypes()
+    }
+    
     render(){
-        return (<Export idDivToPrint="test"><Chart chartData={this.state.mockData}/></Export>)
+        const { loading, documentTypesData, insurancesData, houseTypesData, waterTypesData, heatingTypesData } = this.state
+        
+        let show = <CircularIndeterminate/>
+        if(!loading){
+            show = (
+                <Export idDivToPrint="test">
+                    <Chart chartData={this.state.mockData}/>
+                </Export>
+            )
+        }
+        return show
     }
 }
 
