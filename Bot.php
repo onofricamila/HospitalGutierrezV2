79825b62ec5a4d92f12fc9d4599709e8782dc49e
Onofri Camila <onofricamila2@gmail.com>
2018-06-29 20:31:46 -0300

    reorganizacion de archivos sacando bot y cosas de la v1

--- Bot.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once 'controller/Bot.php';
-BotController::request();
--- Bot/Bot.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once $_SERVER['DOCUMENT_ROOT'].'/controller/Bot.php';
-BotController::request();
